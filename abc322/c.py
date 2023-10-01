82d2ff3f16c276d9926013ec9291996c955bdd95
Johndoe <56298669+togane@users.noreply.github.com>
2023-10-01 10:46:52 +0900

    ABC322 C問 パフォーマンスチューニング
    - 修正前のコードの計算量は O(NM)
      - pop(0) は O(M) の時間がかかるため
    - O(1) で実行できる popleft() を使って書き直す

--- abc322/c.py
+++ abc322/c.py
@@ -1,9 +1,11 @@
+from collections import deque
+
 N, M = map(int, input().split(" "))
-event_days = list(map(int, input().split(" ")))
+event_days = deque(map(int, input().split(" ")))
 
 i = 1
 while len(event_days) > 0:
-    next_event_day = event_days.pop(0)
+    next_event_day = event_days.popleft()
     for day in range(i, next_event_day + 1):
         print(next_event_day - day)
 
